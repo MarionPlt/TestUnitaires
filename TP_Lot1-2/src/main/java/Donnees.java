00ad0e200bfc9ae7f7ccc558aef33035ce6a0a0b
MarionJS <74191216+MarionJS@users.noreply.github.com>
2021-01-15 13:33:18 +0100

    Add files via upload

---
+++ TP_Lot1-2/src/main/java/Donnees.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Donnees {
+
+    public static List<String[]> importFichier(String path) {
+        List<String[]> listeLecture = new ArrayList<String[]>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));     ////Création du FileReader+BufferedReader
+            String s;   //Initilisation de l'objet String utilisé pour la lecture
+            while ((s = br.readLine()) != null) { //On lit chaque ligne jusqu'à ce qu'il n'y en ait plus
+                //Split des chiffres par ";"
+                String[] tab = s.split(";", 4);
+                listeLecture.add(tab);
+            }
+
+        } catch (Exception e) {
+            System.out.println(" Erreur détectée : veuillez vérifier le format de votre fichier. ");
+            System.exit(1);
+        }
+        return listeLecture;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Veuillez indiquer le chemin du fichier sudoku à importer : ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+        String[] tab;
+        List<String[]> listeLecture = new ArrayList<String[]>(importFichier(path));
+
+        for (int i = 0; i < listeLecture.size(); i++) {
+            tab = listeLecture.get(i);
+            for (int j = 0; j < 4; j++) {
+                System.out.print(tab[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
