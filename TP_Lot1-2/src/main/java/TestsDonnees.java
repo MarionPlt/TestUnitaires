d0d4999f3f8e98601f633f7e590d8b4af5970586
MarionJS <marion.playout@gmail.com>
2021-02-25 09:39:29 +0100

    modifications on donnees.java

--- TP_Lot1-2/src/main/java/TestsDonnees.java
+++ TP_Lot1-2/src/main/java/TestsDonnees.java
@@ -1,14 +1,17 @@
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
-public class TestsDonnees extends TestCase {
+public class TestsDonnees {
 
 
     public String nodatapass = "..\\src\\main\\nodata.txt";
@@ -27,12 +30,18 @@ public class TestsDonnees extends TestCase {
         return generatedString;
     }
 
+    public Optional<String> getExtensionByStringHandling(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+    }
+
 
     @Test
     public void testFileExist() {
         String rightpass = ".\\src\\main\\client.txt";
-        List<String[]> test = new ArrayList(Donnees.importFichier(rightpass));
-        assertNotNull(test);
+        ArrayList test = new ArrayList(Donnees.importFichier(rightpass));
+        Assert.assertNotNull(test);
     }
 
     @Test(expected = FileNotFoundException.class)
@@ -43,42 +52,44 @@ public class TestsDonnees extends TestCase {
 
     @Test
     public void testIdUnique() throws SQLException {
-        Client clienttest = new Client(randomId(),"prenomTest", "nomTest", "adresseTest");
+        Client clienttest = new Client(randomId(), "prenomTest", "nomTest", "adresseTest");
         MariaDB database = new MariaDB();
-        String queryTest= "SELECT COUNT(*) FROM clientsunit WHERE ID = '" + clienttest.getId()+"'" ;
-        ResultSet resultat=database.talkToDataBase(queryTest);
+        String queryTest = "SELECT COUNT(*) FROM clientsunit WHERE ID = '" + clienttest.getId() + "'";
+        ResultSet resultat = database.talkToDataBase(queryTest);
         resultat.next();
-        assertTrue(resultat.getInt(1)==0);
+        Assert.assertTrue(resultat.getInt(1) == 0);
     }
 
     @Test
-	public void isNotEmpty() {
-		File file = new File("nodata.txt");
-	  	//assertTrue(file.length() == 0);
-	  	
-	  	assertEquals(file.length() == 0, file.length() == file.length());
-	  	System.out.println("Erreur fichier vide");
-	  	}
-    
+    public void isNotEmpty() {
+        File file = new File("nodata.txt");
+        //assertTrue(file.length() == 0);
+
+        Assert.assertEquals(file.length() == 0, file.length() == file.length());
+        System.out.println("Erreur fichier vide");
+    }
+
     @Test
-	public void badFormat() {
-		
-		File file = new File("client.");
-		Optional<String> value1 = Optional.of(""); 
-	getExtensionByStringHandling(file.toString());
-	Optional<String> value = getExtensionByStringHandling(file.toString());
-	assertEquals(value, value1);
-	System.out.println("ERREUR MAUVAIS FORMAT : ATTENDU - .txt");
-	}
-	
-	@Test
-	public void goodFormat() {
-		
-		File file = new File("client.txt");
-		Optional<String> value1 = Optional.of("txt"); 
-	getExtensionByStringHandling(file.toString());
-	Optional<String> value = getExtensionByStringHandling(file.toString());
-	assertEquals(value , value1);
-	//System.out.println("ERREUR MAUVAIS FORMAT : ATTENDU - .txt");
-	}
+    public void badFormat() {
+
+        File file = new File("client.");
+        Optional<String> value1 = Optional.of("");
+        getExtensionByStringHandling(file.toString());
+        Optional<String> value = getExtensionByStringHandling(file.toString());
+        Assert.assertEquals(value, value1);
+        System.out.println("ERREUR MAUVAIS FORMAT : ATTENDU - .txt");
+    }
+
+
+
+    @Test
+    public void goodFormat() {
+
+        File file = new File("client.txt");
+        Optional<String> value1 = Optional.of("txt");
+        getExtensionByStringHandling(file.toString());
+        Optional<String> value = getExtensionByStringHandling(file.toString());
+        Assert.assertEquals(value, value1);
+        System.out.println("ERREUR MAUVAIS FORMAT : ATTENDU - .txt");
+    }
 }
